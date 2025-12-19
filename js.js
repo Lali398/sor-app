69618b1c960205498da682ec7a8937aa51a5bc5a
Lali398 <korsoslala@gmail.com>
2025-12-19 16:17:00 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    
+    if (typeof Chart !== 'undefined') {
+        Chart.defaults.color = '#e0e0e0';
+        Chart.defaults.borderColor = 'rgba(255, 255, 255, 0.2)';
+    }
     
     // --- NÉZETEK ÉS ELEMEK ---
     // --- KURZOR ELEMEK ÉS LOGIKA ---
@@ -3233,6 +3236,7 @@ window.closeRecoveryModal = function() {
     }, 300);
 }
 });
+
 
 
 
