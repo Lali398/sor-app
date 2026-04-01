58ff5806f1bf75133f53f5f7f5938d3736a47d31
Lali398 <korsoslala@gmail.com>
2026-04-01 18:47:39 +0200

    Update service worker to version 7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME        = 'beer-app-v5';
-const API_CACHE_NAME    = 'beer-api-cache-v1';
+const CACHE_NAME        = 'beer-app-v7';
+const API_CACHE_NAME    = 'beer-api-cache-v3';
 const SYNC_QUEUE_DB     = 'beer-sync-queue';
 const SYNC_TAG          = 'beer-sync-offline';
 
